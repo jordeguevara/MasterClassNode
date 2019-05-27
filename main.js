91f9e9837bd956c1444b4c36274d6f1a4f823eaa
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-05-26 18:21:19 -0700

    update

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
-//Dependencies 
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
-const http = require('http');
-const url = require('url');
-const stringDecoder = require('string_decoder').StringDecoder;
+const server = http.createServer((req, res) => {
+  let parsedURL = url.parse(req.url, true);
 
-const server = http.createServer((request,response)=>{
-    //Parse URL
-    let parsedURL = url.parse(request.url,true);
-    //get Path
-    let path = parsedURL.pathname; // untrimmed
+  const path = parsedURL.path;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const query = parsedURL.query;
+  const headers = req.headers;
+  const method = req.method.toLowerCase();
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", data => {
+    console.log(data);
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
 
-    //Parse HTTP Method
-    let method = request.method.toUpperCase();
-
-    let queryStringObject = parsedURL.query;
-
-    //remove extra '/'s
-    let trimmedPath = path.replace(/\/+|\/+$/g,'');
-
-    //getHeader
-    let headers = request.headers;
-
-    //getPayload
-    let decoder = new StringDecoder('utf-8');
-    let buffer ='';
-    request.on('data', (data)=>{
-        buffer += decoder.write(data);
-    })
-    request.on('end', ()=>{
-        buffer += decoder.end();
-        console.log(`Request received with this payload ${buffer}`);
-        response.end('Sucess Data');
-    }
-
-
-   
+    res.end("hello world");
+  });
 });
 
-server.listen(port=3000,()=>{
-    console.log(`server is listening on port ${port}`);
-})
+server.listen(3000, () => {
+  console.log("Server is listen on port 3000");
+});
